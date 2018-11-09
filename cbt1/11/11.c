70136bab67a826b22908cc8f3c2d5e5aea37e0de
JeffyuehLin <38328932+JeffyuehLin@users.noreply.github.com>
2018-11-09 15:56:36 +0800

    Update 11.c

--- cbt1/11/11.c
+++ cbt1/11/11.c
@@ -38,13 +38,13 @@ void heap_sort(int arr[], int len)
     int i;
     //初始化，i從最後一個父節點開始調整
     for (i = len / 2 - 1; i >= 0; i--)
-        max_heapify(arr, i, len - 1);
+        min_heapify(arr, i, len - 1);
     printarr(arr, len);
     //先將第一個元素和已排好元素前一位做交換，再重新調整，直到排序完畢
     for (i = len - 1; i > 0; i--)
     {
         swap(&arr[0], &arr[i]);
-        max_heapify(arr, 0, i - 1);
+        min_heapify(arr, 0, i - 1);
         printarr(arr, i);
     }
 }
